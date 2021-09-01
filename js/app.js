37bf4dfcb01c6bde4582e1b654f6042fc2216767
mukitur <mukit.cpl@gmail.com>
2021-09-01 13:42:39 +0600

    updates

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function loadCocktailsName() {
 }
 
 const displayCockTails = drinks => {
-    console.log(drinks);
+    // console.log(drinks);
     const displayCocktail = document.getElementById('display-cocktail');
     displayCocktail.textContent = '';
     for (const drink of drinks) {
@@ -30,9 +30,44 @@ const displayCockTails = drinks => {
                     <h5 class="card-title">${drink.strDrink}</h5>
                     <p class="card-text">Ingradients: ${drink.strIngredient1}, ${drink.strIngredient2}, ${drink.strIngredient3},${drink.strIngredient4}, ${drink.strIngredient5}</p>
                 </div>
-                <button class="btn btn-info">Details ${drink.strDrink}</button>
+                <button onclick= "loadCocktailDetails(${drink.idDrink})" class="btn btn-info">More About ${drink.strDrink}</button>
             </div>
         `;
         displayCocktail.appendChild(div);
     }
+}
+
+const loadCocktailDetails = idDrink => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+    //console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCocktailDetails(data.drinks[0]));
+}
+
+const displayCocktailDetails = drinks => {
+    const cocktailDetails = document.getElementById('get-cocktail-details');
+    cocktailDetails.textContent = '';
+    const detailsDIV = document.createElement('detailsDIV');
+    detailsDIV.classList.add('row', 'gy-5',)
+    detailsDIV.innerHTML = `
+        <div class="col-6">
+             <div class="p-2 bg-light img-fluid rounded mx-auto">
+                <img width = "600px" src="${drinks.strDrinkThumb}">
+             </div>   
+        </div>
+        <div class="col-6">
+            <div class="p-3 border bg-white">
+                <h5>Ingredients</h5>
+                <div><img width = "200px" src="${drinks.strDrinkThumb}"></br> ${drinks.strCategory} </div
+                <div></div
+                <div></div
+            </div>
+        </div>
+        <div class="col-12 border">
+            <div class="p-3  bg-white text-center mx-auto"> Instructions: </br> ${drinks.strInstructions}</div>
+            <div class="p-3  bg-white text-center mx-auto"> Glass: </br> ${drinks.strGlass}</div>
+        </div>
+    `;
+    cocktailDetails.appendChild(detailsDIV);
 }
